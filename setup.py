83789cfee39ebece3e173b766ae3db9254d2983f
Michael E. Karpeles <michael.karpeles@gmail.com>
2012-09-27 03:19:16 -0700

    Committing scaffolding + docs for the ohp project, a collection of interview questions

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ohp',
+    version='0.1.2',
+    author='mek',
+    author_email='michael.karpeles@gmail.com',
+    packages=['ohp', 'ohp.test'],
+    scripts=[],
+    url='',
+    license='LICENSE',
+    long_description=open('README').read(),
+)
